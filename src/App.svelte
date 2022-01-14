692f1640e99cb8e444f61b9dad63b2aaf12c835a
GamingInfinite <ender2gamer123@gmail.com>
2022-01-14 07:13:38 -0600

    Initial Work

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { Container, Row, Col } from "sveltestrap";
+</script>
+
+<div class="App">
+  <Container class="g-0 container-fluid">
+    <Row class="align-items-center" style="height: 100vh;">
+      <Col class="">
+        <div class="d-flex justify-content-center align-items-center panel">Player</div>
+      </Col>
+      <Col class="">
+        <div class="d-flex justify-content-center align-items-center panel">Dungeon Master</div>
+      </Col>
+    </Row>
+  </Container>
+</div>
